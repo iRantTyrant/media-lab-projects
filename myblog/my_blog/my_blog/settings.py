55235d5ef0bc4326a00f13682f44e01122f6af9d
iRantTyrant <104159559+iRantTyrant@users.noreply.github.com>
2025-04-18 21:29:19 +0300

    Added an env  dependency using python decouple

--- myblog/my_blog/my_blog/settings.py
+++ myblog/my_blog/my_blog/settings.py
@@ -123,10 +123,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com' 
-EMAIL_PORT = 587  
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'test@gmail.com'  # Replace with your actual email address
-EMAIL_HOST_PASSWORD = 'password'  # Replace with your actual password
+from decouple import config # Use decouple to manage sensitive information
 
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
